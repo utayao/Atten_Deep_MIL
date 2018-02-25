cf9e41f3b5879a8e43275b558817d3af842ad330
Jiawen Yao <yjiawenee@gmail.com>
2018-02-24 18:40:56 -0600

    Add files via upload

---
+++ utl/dataset.py
@@ -0,0 +1,32 @@
+import numpy as np
+import glob
+from sklearn.cross_validation import KFold
+
+def load_dataset(dataset_path, n_folds, rand_state):
+    """
+    Parameters
+    --------------------
+    :param dataset_path:
+    :param n_folds:
+    :return: list
+        List contains split datasets for K-Fold cross-validation
+    """
+
+    # load datapath from path
+    pos_path = glob.glob(dataset_path+'/1/img*')
+    neg_path = glob.glob(dataset_path+'/0/img*')
+
+    pos_num = len(pos_path)
+    neg_num = len(neg_path)
+
+    all_path = pos_path + neg_path
+
+    num_bag = len(all_path)
+    kf = KFold(num_bag, n_folds=n_folds, shuffle=True, random_state=rand_state)
+    datasets = []
+    for train_idx, test_idx in kf:
+        dataset = {}
+        dataset['train'] = [all_path[ibag] for ibag in train_idx]
+        dataset['test'] = [all_path[ibag] for ibag in test_idx]
+        datasets.append(dataset)
+    return datasets
